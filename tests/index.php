c8aaf88738531fc4ce567829141d1e97735b5bdd
ybluesky <xjz1688@163.com>
2018-01-24 17:56:16 +0800

    add ikanalyzer test

--- tests/index.php
+++ tests/index.php
@@ -29,6 +29,38 @@ class Index extends \SwaSky\ElasticSearch\ElasticSearch
         $res = $this->indexDelete();
         print_r($res);
     }
+
+    /**
+     * 创建索引时创建分词器
+     */
+    public function ikanalyzer()
+    {
+        $res = $this->indexCreate(1,0,
+            [
+                'analysis' => [ //设置分析器
+                    'analyzer' => [
+                        'ik_analyzer' => [//自定义分词器
+                            'tokenizer' => 'ik_analyzer_selecter',
+                            "char_filter" => ["html_strip"] //过滤html标签
+                        ]
+                    ],
+                    'tokenizer' => [ //设置分词
+                        'ik_analyzer_selecter' => [
+                            'type' => 'ik_smart'
+                        ]
+                    ]
+                ]
+            ],
+            [
+                'title' => [ //名称
+                    'type' => 'text',
+                    'analyzer' => 'ik_analyzer', //中文分词
+                    'search_analyzer' => 'ik_analyzer',
+                ]
+            ]
+        );
+        print_r($res);
+    }
 }
 
 $type = $argv[1]; //测试类型
